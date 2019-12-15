bd917b74cbe732a269b052eed450a30483af9d00
waspryo <wasp2123321@gmail.com>
2019-12-15 14:40:46 +0900

    map sub

--- index.js
+++ index.js
@@ -4,3 +4,19 @@ const doubled = numbers.map((number) => {
     return number * 2;
 });
 console.log(doubled);
+
+const days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Fridayday',
+    'Saturday'
+]
+
+const abreviated = days.map((day) => {
+    return day.substring(0, 3);
+})
+
+console.log(abreviated)
